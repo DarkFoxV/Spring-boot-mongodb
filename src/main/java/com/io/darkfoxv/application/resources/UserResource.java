aa4211d1f03f186f4e2efb71647317efd15b9de8
Michel Pereira <michelpereira4410@gmail.com>
2024-05-06 16:33:37 -0300

    Obtendo um usuário por i

--- src/main/java/com/io/darkfoxv/application/resources/UserResource.java
+++ src/main/java/com/io/darkfoxv/application/resources/UserResource.java
@@ -6,10 +6,12 @@ import com.io.darkfoxv.application.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -17,11 +19,19 @@ import java.util.stream.Collectors;
 public class UserResource {
     @Autowired
     private UserService userService;
+
     @GetMapping
-    public ResponseEntity<List<UserDTO>>  findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> users = userService.findAll();
         List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).toList();
         return ResponseEntity.ok().body(usersDTO);
 
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = userService.findById(id);
+        UserDTO obj = new UserDTO(user);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/io/darkfoxv/application/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.io.darkfoxv.application.resources.exceptions;
+
+import com.io.darkfoxv.application.services.exception.ObjectNotFoundException;
+import com.io.darkfoxv.application.services.exception.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        String error = "Object not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
